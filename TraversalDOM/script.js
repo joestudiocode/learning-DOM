1fc31c82c53b59b52a67d7a1e75df81b5b093372
joeStudioCode <joestudiocode@gmail.com>
2023-07-31 18:56:30 +0700

    Add new learning list Tranversal DOM and prevent default

---
+++ TraversalDOM/script.js
@@ -0,0 +1,44 @@
+//Event Handling
+
+// const cardFull = document.querySelector(".card");
+// const closeRed = document.querySelector(".close");
+
+// closeRed.addEventListener("click", function () {
+//   cardFull.style.display = "none";
+// });
+
+//Dom Traversal
+
+const close = document.querySelectorAll(".close");
+const card = document.querySelectorAll(".card");
+// console.log(close)
+
+// for (let i = 0; i < close.length; i++) {
+//   close[i].addEventListener("click", function (e) {
+//     // alert("tombol ke " + i)
+//     card[i].style.display = "none";
+//     // e.target.parentElement.style.display = "none";
+//   });
+// }
+
+close.forEach(function (param) {
+  param.addEventListener("click", function (e) {
+    e.target.parentElement.parentElement.style.display = "none";
+    e.preventDefault();
+  });
+});
+
+//preventdefault  is the method that works for prevent happening default events from a DOM, example the tag 'a' if we click, then the browser page will be reload, or a form if we click submit button then will be reaload to.
+
+
+//if use parentElement we can pile it up, up to grandfather
+
+// {
+//   ("Method");
+//   //parentNOde
+//   //parentElement
+//   //nextSibling
+//   //nextElementSibling ----- use this
+//   //previousSibling
+//   //previousElementSibling ------ use this
+// }
